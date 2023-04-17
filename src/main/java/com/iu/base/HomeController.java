cbd8353e36ba1e98b1482a6591cf2dd773317cdb
tjrgustn06 <tjrgustn06@naver.com>
2023-04-17 16:46:05 +0900

    qwe

---
+++ src/main/java/com/iu/base/HomeController.java
@@ -0,0 +1,13 @@
+package com.iu.base;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/index")
+	public String home() {
+		return "index";
+	}
+}
